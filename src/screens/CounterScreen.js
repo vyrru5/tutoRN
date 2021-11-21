7533e816460fbdb575a1821ddab0d2b6ccdf4c65
alex <vyrru5@users.noreply.github.com>
2021-11-21 19:49:31 +0100

    modification button

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,5 +1,8 @@
 import React, {useReducer}  from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View,TouchableOpacity } from 'react-native'
+import { FontAwesome } from '@expo/vector-icons';
+import tailwind from "tailwind-rn";
+
 
 
 
@@ -14,22 +17,34 @@ switch(action.type){
 
 }
 
+
 const CounterScreen = () => {
 
     const [state, dispatch] = useReducer(reducer, {count :0})
 
 
     return (
-        <View>
-             <Button title='reset' onPress={ () => { dispatch({type : 'reset', payload : 0}) }} />
-            <Button title='increase' onPress={ () => { dispatch({type : 'increase', payload : 1}) }} />
-            <Button title='decrease' onPress={ () => { dispatch({type : 'decrease', payload : 1}) }}/>
-           
-            <View style={styles.container}>
-                <Text>Nombre</Text>
-            <Text style={styles.bigNumber}>{state.count}</Text>
-            </View>
-        </View>
+    <View >
+    {/*            
+    <View style={styles.wrapper}>
+        <Button title='reset' onPress={ () => { dispatch({type : 'reset', payload : 0}) }} />
+        <Button title='increase' onPress={ () => { dispatch({type : 'increase', payload : 1}) }} />
+        <Button title='decrease' onPress={ () => { dispatch({type : 'decrease', payload : 1}) }} />
+        </View> 
+        */}
+
+    <View style={tailwind('flex-row justify-between m-2')}>
+        <FontAwesome.Button name="minus-square-o" backgroundColor="#3b5998" style={tailwind('text-center flex items-center')} onPress={ () => { dispatch({type : 'decrease', payload : 1}) }}>Diminuer</FontAwesome.Button>
+        <FontAwesome.Button name="refresh" backgroundColor="#3b5998" style={tailwind('text-center flex items-center')} onPress={ () => { dispatch({type : 'reset', payload : 0}) }} >reset</FontAwesome.Button>
+        <FontAwesome.Button name="plus-square-o" backgroundColor="#3b5998" style={tailwind('text-center flex items-center')} onPress={ () => { dispatch({type : 'increase', payload : 1}) }} >Augmenter</FontAwesome.Button>
+    </View>
+
+    <View style={styles.container}>
+        <Text>Nombre</Text>
+    <Text style={styles.bigNumber}>{state.count}</Text>
+    </View>
+        
+    </View>
     )
 }
 
@@ -40,10 +55,17 @@ const styles = StyleSheet.create({
         alignItems:'center',
         flexDirection: 'column',
         justifyContent:'center',
-        backgroundColor: '#ececec'
+       
+
+
+    },
+    wrapper:{
+        justifyContent:'space-between',
+        padding:5,  
     },
     bigNumber:{
-        color:'red',
-        fontSize:45
-    }
+        color:'#3b5998',
+        fontSize:45,
+    },
+
 })
